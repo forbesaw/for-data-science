480e72391b312b7d8b68e2ab1ff63f26df0d52e5
Aaron Forbes <aaron.forbes@coinbase.com>
2019-10-05 12:24:54 -0700

    changes

--- fiscal_decentralization_stata.py
+++ fiscal_decentralization_stata.py
@@ -1,11 +1,3 @@
 import pandas
-data = pandas.read_stata("/Users/aaronforbes/for-date-science/raw_data/fiscal_decentralization/time_series.csv", delimiter = ',')
-new_data = data.rename(columns = {"Country Name":"country_name", ## easier to read
-                              "Country Code": "country_code",
-                                  "Indicator Name": "indicator_name",
-                                  "Indicator Code": "indicator_code",
-                                  "Sector Name": "sector_name",
-                                  "Sector Code": "sector_code",
-                                  "Attribute": "attribute"})
-filtered_data = new_data.dropna(axis='columns', how='all').dropna(axis=1, how='all') ## remove null columns
-print(filtered_data)
+data = pandas.read_stata("/Users/aaronforbes/for-date-science/raw_data/fiscal_decentralization_stata/fiscal_decentralization_stata_file.dta")
+print(data)
